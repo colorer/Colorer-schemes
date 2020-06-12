be6cba243803168f24aa160cdb0f4ff788f7403c
Aleksey Dobrunov <ctapmex@gmail.com>
2020-06-12 19:28:17 +0500

    python: add underscores in numeric literals

---
+++ hrc/test/python/number.py
@@ -0,0 +1,10 @@
+decimal = 10_000
+float_number = 3.14_15_92
+float_number2 =.1245_44E-10
+float_number3 = 1e100
+hex_number = 0xCAFE_BABE
+hex_number = 0x_CAFE
+oct_number = 0o_755
+bin_number = 0b111_000_111
+im_number = 3.14_15_93j
+
