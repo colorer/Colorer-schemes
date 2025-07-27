cbc7495db94ee450835e57639c126a5538b06595
Aleksey Dobrunov <ctapmex@ctapmex.com>
2025-07-27 21:11:42 +0500

    check for known error on test

--- tests/test_fullload.sh
+++ tests/test_fullload.sh
@@ -8,7 +8,19 @@ fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 current_date=$(date +"%Y-%m-%d_%H-%M-%S")
+LOG_FILE=${SCRIPT_DIR}/../${RESULT_DIR}/colorer_${current_date}.log 
 
 mkdir -p "${SCRIPT_DIR}/../_test"
 
 ${SCRIPT_DIR}/../bin/colorer -c ${SCRIPT_DIR}/../_build/${BASE}/catalog.xml -ll -el info -eh colorer_${current_date} -ed ${SCRIPT_DIR}/../${RESULT_DIR}
+
+# checking for new errors
+grep -v -F -f ${SCRIPT_DIR}/ignored_error.txt ${LOG_FILE}
+
+if [ $? -eq 1 ]; then
+  echo "✅ Success: there are no unknown errors in the log."
+else
+  echo "❌ Error: unknown errors were found in the log file!"
+  echo "Found lines:"
+  grep --color=auto -F -f ${SCRIPT_DIR}/ignored_error.txt ${LOG_FILE}
+fi
