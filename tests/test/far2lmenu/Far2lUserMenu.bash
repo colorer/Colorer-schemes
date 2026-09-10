a71f42e3af857bc7826d41373ef07db1c4275881
Aleksey Dobrunov <ctapmex@ctapmex.com>
2026-09-10 22:46:54 +0500

    add far2l user menu

---
+++ tests/test/far2lmenu/Far2lUserMenu.bash
@@ -0,0 +1,9 @@
+ls -la "!.!"
+grep !?Search for:?! !?In:?*.*!|far2l -v -
+grep !?$GrepHist$Search for:?! !?In:?*.*!
+grep !?Find in (!.!):?! |far2l -v -
+[ -f !##!/!^!.! ] && diff -c -p !##!/!^!.! !/!.!
+cat !@@AFQ! !$!
+echo !! !=/ !` !&
+@rm -f !.!
+printf '%s\n' '!/'
